57e8232a6c030e97c47065f60adf7e00b1b2ccb5
Jukka Svahn <web@rahinaa.biz>
2013-07-15 15:06:01 +0300

    Updated programming style.

--- inc/fetch_cached.php
+++ inc/fetch_cached.php
@@ -9,77 +9,84 @@
  * @example rah_cache_init(array $options);
  */
 
-	function rah_cache_init($opt) {
-		
-		global $rah_cache;
-		$rah_cache = $opt;
+    function rah_cache_init($opt)
+    {
+        global $rah_cache;
+        $rah_cache = $opt;
 
-		if(txpinterface != 'public' || !empty($_POST) || !empty($_GET) || !empty($_COOKIE['txp_login_public'])) {
-			return;
-		}
-		
-		$request_uri = trim($_SERVER['REQUEST_URI'], '/');
-		$md5 = md5($request_uri);
-		$filename = $file = $rah_cache['path'] . '/' . $md5 . '.rah';
-		$encoding = rah_cache_encoding();
-		
-		if($encoding) {
-			$filename = $file . '.gz';
-		}
-		
-		if(file_exists($filename)) {
-		
-			$modified = filemtime($filename);
-		
-			if(
-				$modified > time()-2592000 && 
-				$modified >= (int) @file_get_contents($rah_cache['path'] . '/_lastmod.rah')
-			) {
-				header('Content-type: text/html; charset=utf-8');
-				
-				if($encoding) {
-					header('Content-Encoding: '.$encoding);
-				}
-				
-				die(file_get_contents($filename));
-			}
-		}
-		
-		if(
-			!file_exists($rah_cache['path']) || 
-			!is_dir($rah_cache['path']) || 
-			!is_writeable($rah_cache['path'])
-		) {
-			return;
-		}
-		
-		$rah_cache['file'] = $file;
-		$rah_cache['request_uri'] = $request_uri;
-		$rah_cache['cache_key'] = $md5;
-	}
+        if (txpinterface != 'public' || !empty($_POST) || !empty($_GET) || !empty($_COOKIE['txp_login_public']))
+        {
+            return;
+        }
+
+        $request_uri = trim($_SERVER['REQUEST_URI'], '/');
+        $md5 = md5($request_uri);
+        $filename = $file = $rah_cache['path'] . '/' . $md5 . '.rah';
+        $encoding = rah_cache_encoding();
+
+        if ($encoding)
+        {
+            $filename = $file . '.gz';
+        }
+
+        if (file_exists($filename))
+        {
+            $modified = filemtime($filename);
+
+            if (
+                $modified > time()-2592000 && 
+                $modified >= (int) @file_get_contents($rah_cache['path'] . '/_lastmod.rah')
+            )
+            {
+                header('Content-type: text/html; charset=utf-8');
+                
+                if ($encoding)
+                {
+                    header('Content-Encoding: '.$encoding);
+                }
+
+                die(file_get_contents($filename));
+            }
+        }
+
+        if (
+            !file_exists($rah_cache['path']) || 
+            !is_dir($rah_cache['path']) || 
+            !is_writeable($rah_cache['path'])
+        )
+        {
+            return;
+        }
+
+        $rah_cache['file'] = $file;
+        $rah_cache['request_uri'] = $request_uri;
+        $rah_cache['cache_key'] = $md5;
+    }
 
 /**
- * Check accepted encoding headers
+ * Check accepted encoding headers.
+ *
  * @return bool
  */
 
-	function rah_cache_encoding() {
-	
-		if(!isset($_SERVER['HTTP_ACCEPT_ENCODING']) || headers_sent()) {
-			return false;
-		}
-	
-		$accept_encoding = $_SERVER['HTTP_ACCEPT_ENCODING'];
-		
-		if(strpos($accept_encoding, 'x-gzip') !== false) {
-			return 'x-gzip';
-		}
-		
-		if(strpos($accept_encoding, 'gzip') !== false) {
-			return 'gzip';
-		}
-		
-		return false;
-	}
+    function rah_cache_encoding()
+    {
+        if (!isset($_SERVER['HTTP_ACCEPT_ENCODING']) || headers_sent())
+        {
+            return false;
+        }
 
-?>
+        $accept_encoding = $_SERVER['HTTP_ACCEPT_ENCODING'];
+
+        if (strpos($accept_encoding, 'x-gzip') !== false)
+        {
+            return 'x-gzip';
+        }
+
+        if (strpos($accept_encoding, 'gzip') !== false)
+        {
+            return 'gzip';
+        }
+
+        return false;
+    }
--- rah_cache.php
+++ rah_cache.php
@@ -3,189 +3,218 @@
 /**
  * Rah_cache plugin for Textpattern CMS.
  *
- * @author Jukka Svahn
- * @date 2012-
+ * @author  Jukka Svahn
  * @license GNU GPLv2
- * @link https://github.com/gocom/rah_cache
+ * @link    https://github.com/gocom/rah_cache
  * 
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
- * Licensed under GNU Genral Public License version 2
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
+ * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	new rah_cache();
-
 /**
- * A tag to control caching on a page basis
+ * A tag to control caching on a page basis.
+ *
+ * @param  array $atts
+ * @return string
  */
 
-	function rah_cache($atts) {
-		
-		global $rah_cache;
-		
-		extract(lAtts(array(
-			'ignore' => 0
-		), $atts));
-		
-		if($ignore) {
-			$rah_cache['file'] = null;
-		}
-	}
+    function rah_cache($atts)
+    {
+        global $rah_cache;
+
+        extract(lAtts(array(
+            'ignore' => 0,
+        ), $atts));
+
+        if ($ignore)
+        {
+            $rah_cache['file'] = null;
+        }
+    }
 
 /**
- * Cache handler
+ * Cache handler.
  */
 
-class rah_cache {
+class rah_cache
+{
+    /**
+     * The page data.
+     *
+     * @var string
+     */
 
-	static public $data;
-	
-	/**
-	 * @var array Stores sent HTTP response headers
-	 */
-	
-	protected $headers = array();
-	
-	/**
-	 * Constructor
-	 */
-	
-	public function __construct() {
-		global $event;
-		register_callback(array($this, 'store'), 'textpattern_end');
-		register_callback(array($this, 'update_lastmod'), $event ? $event : 'textpattern_end');
-	}
+    static public $data;
 
-	/**
-	 * Sets page data
-	 * @param $name
-	 */
-	
-	static public function data($data=NULL) {
-		if($data !== NULL) {
-			self::$data = $data;
-		}
-		
-		return self::$data;
-	}
-	
-	/**
-	 * Gets sent response headers
-	 */
-	
-	protected function get_headers() {
-		
-		if(!function_exists('header_list') || !header_list()) {
-			return;
-		}
-		
-		foreach((array) header_list() as $header) {
-			if(strpos($header, ':')) {
-				$header = explode(':', strtolower($header), 2);
-				$this->headers[trim($header[0])] = trim($header[1]);
-			}
-		}
-	}
+    /**
+     * Stores sent HTTP response headers.
+     *
+     * @var array 
+     */
 
-	/**
-	 * Writes the page to cache directory
-	 */
-	
-	public function store() {
-		
-		global $prefs, $rah_cache;
-		
-		if(empty($rah_cache['file']) || $prefs['production_status'] != 'live') {
-			return;
-		}
+    protected $headers = array();
 
-		if(!empty($rah_cache['skip'])) {
-			foreach((array) $rah_cache['skip'] as $pattern) {
-				if(strpos($rah_cache['request_uri'], $pattern) === 0) {
-					return;
-				}
-			}
-		}
-		
-		$this->get_headers();
-		
-		if(
-			isset($this->headers['content-type']) &&
-			strpos($this->headers['content-type'], 'text/html') === false
-		) {
-			return;
-		}
-		
-		self::$data = ob_get_contents();
-		
-		callback_event('rah_cache.store');
-		
-		if(!self::$data) {
-			return;
-		}
-		
-		file_put_contents($rah_cache['file'], self::$data);
-		
-		if(function_exists('gzcompress')) {
-			$size = strlen(self::$data);
-			$crc = crc32(self::$data);
-			$data = gzcompress(self::$data, 6);
-			$data = substr($data, 0, strlen($data)-4);
-			$data = "\x1f\x8b\x08\x00\x00\x00\x00\x00".$data;
-			$data .= pack('V', $crc);
-			$data .= pack('V', $size);
-			file_put_contents($rah_cache['file'].'.gz', $data);
-		}
+    /**
+     * Constructor.
+     */
 
-		callback_event('rah_cache.created');
-	}
-	
-	/**
-	 * Update lastmod
-	 */
+    public function __construct()
+    {
+        global $event;
+        register_callback(array($this, 'store'), 'textpattern_end');
+        register_callback(array($this, 'update_lastmod'), $event ? $event : 'textpattern_end');
+    }
 
-	public function update_lastmod() {
-		global $rah_cache;
-		
-		if(!empty($rah_cache['path'])) {
-			file_put_contents(
-				$rah_cache['path'] . '/_lastmod.rah', @strtotime(get_pref('lastmod', 'now', true))
-			);
-		}
-	}
+    /**
+     * Sets page data.
+     *
+     * @param $name
+     */
 
-	/**
-	 * Flush cache
-	 * @param string|array $file
-	 * @return bool
-	 */
-	
-	static public function flush($file) {
-		
-		global $rah_cache;
-		
-		if($file !== NULL) {
-			
-			foreach((array) $file as $f) {
-		
-				if(!preg_match('#[0-9a-f]{32}$#i', $f)) {
-					return false;
-				}
-				
-				$f = $rah_cache['path'] . '/' . $f;
-				
-				unlink($f . '.rah');
-				unlink($f . '.rah.gz');
-			}
-			
-			return true;
-		}
-		
-		foreach((array) glob($rah_cache['path'].'/*', GLOB_NOSORT) as $file) {
-			if(is_file($file) && strlen(basename($file)) == 32) {
-				unlink($file);
-			}
-		}
-	}
+    static public function data($data = null)
+    {
+        if ($data !== null)
+        {
+            self::$data = $data;
+        }
+
+        return self::$data;
+    }
+
+    /**
+     * Gets sent response headers.
+     */
+
+    protected function get_headers()
+    {
+        if (!function_exists('header_list') || !header_list())
+        {
+            return;
+        }
+
+        foreach ((array) header_list() as $header)
+        {
+            if (strpos($header, ':'))
+            {
+                $header = explode(':', strtolower($header), 2);
+                $this->headers[trim($header[0])] = trim($header[1]);
+            }
+        }
+    }
+
+    /**
+     * Writes the page to the cache directory.
+     */
+
+    public function store()
+    {   
+        global $prefs, $rah_cache;
+
+        if (empty($rah_cache['file']) || $prefs['production_status'] != 'live')
+        {
+            return;
+        }
+
+        if (!empty($rah_cache['skip']))
+        {
+            foreach ((array) $rah_cache['skip'] as $pattern)
+            {
+                if (strpos($rah_cache['request_uri'], $pattern) === 0)
+                {
+                    return;
+                }
+            }
+        }
+
+        $this->get_headers();
+
+        if (
+            isset($this->headers['content-type']) &&
+            strpos($this->headers['content-type'], 'text/html') === false
+        )
+        {
+            return;
+        }
+
+        self::$data = ob_get_contents();
+
+        callback_event('rah_cache.store');
+
+        if (!self::$data)
+        {
+            return;
+        }
+
+        file_put_contents($rah_cache['file'], self::$data);
+
+        if (function_exists('gzcompress'))
+        {
+            $size = strlen(self::$data);
+            $crc = crc32(self::$data);
+            $data = gzcompress(self::$data, 6);
+            $data = substr($data, 0, strlen($data)-4);
+            $data = "\x1f\x8b\x08\x00\x00\x00\x00\x00".$data;
+            $data .= pack('V', $crc);
+            $data .= pack('V', $size);
+            file_put_contents($rah_cache['file'].'.gz', $data);
+        }
+
+        callback_event('rah_cache.created');
+    }
+
+    /**
+     * Update last modification timestamp.
+     */
+
+    public function update_lastmod()
+    {
+        global $rah_cache;
+
+        if (!empty($rah_cache['path']))
+        {
+            file_put_contents(
+                $rah_cache['path'] . '/_lastmod.rah', @strtotime(get_pref('lastmod', 'now', true))
+            );
+        }
+    }
+
+    /**
+     * Flushes the cache.
+     *
+     * @param  string|array $file
+     * @return bool
+     */
+
+    static public function flush($file)
+    {
+        global $rah_cache;
+
+        if ($file !== null)
+        {
+            foreach ((array) $file as $f)
+            {
+                if (!preg_match('#[0-9a-f]{32}$#i', $f))
+                {
+                    return false;
+                }
+
+                $f = $rah_cache['path'] . '/' . $f;
+                unlink($f . '.rah');
+                unlink($f . '.rah.gz');
+            }
+
+            return true;
+        }
+
+        foreach ((array) glob($rah_cache['path'].'/*', GLOB_NOSORT) as $file)
+        {
+            if (is_file($file) && strlen(basename($file)) == 32)
+            {
+                unlink($file);
+            }
+        }
+    }
 }
-?>
+
+new rah_cache();
