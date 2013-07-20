933a2b6f5f33d3766a7689053731b558f36f34db
Jukka Svahn <web@rahinaa.biz>
2013-07-20 23:38:12 +0300

    Would that work better.

--- src/Rah/Cache/Handler.php
+++ src/Rah/Cache/Handler.php
@@ -24,7 +24,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-include_once './Config.php';
+include_once dirname(__FILE__).'/Config.php';
 
 /**
  * Cache handler.
